9daee0af535aaeb6da63b63c4bf0378cdde3d013
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-08-02 21:57:22 +0530

    updated tableHeader | added sort icon

--- src/components/common/tableHeader.jsx
+++ src/components/common/tableHeader.jsx
@@ -12,6 +12,13 @@ class TableHeader extends Component {
     this.props.onSort(sortColumn);
   };
 
+  renderSortIcon = (column) => {
+    const { sortColumn } = this.props;
+    if (column.path !== sortColumn.path) return null;
+    if (sortColumn.order === "asc") return <i className="fa fa-sort-asc"></i>;
+    return <i className="fa fa-sort-desc"></i>;
+  };
+
   render() {
     return (
       <thead>
@@ -23,6 +30,7 @@ class TableHeader extends Component {
               style={column.path ? { cursor: "pointer" } : {}}
             >
               {column.label}
+              {this.renderSortIcon(column)}
             </th>
           ))}
         </tr>
