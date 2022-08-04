775390c7a7a94b0b990f00a5de23573ce4c2d99e
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-08-05 01:39:32 +0530

    added registerForm

---
+++ src/components/registerForm.jsx
@@ -0,0 +1,35 @@
+import Form from "./common/form";
+import Joi from "joi-browser";
+
+class RegisterForm extends Form {
+  state = {
+    data: { username: "", password: "", name: "" },
+    errors: {},
+  };
+
+  schema = {
+    username: Joi.string().required().label("Username").email(),
+    password: Joi.string().required().label("Password").min(5),
+    name: Joi.string().required().label("Name"),
+  };
+
+  doSubmit = () => {
+    console.log("Registered");
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Register</h1>
+        <form onSubmit={this.handleSubmit}>
+          {this.renderInput("username", "Username", "text", true)}
+          {this.renderInput("password", "Password", "password")}
+          {this.renderInput("name", "Name")}
+          {this.renderButton("Register")}
+        </form>
+      </div>
+    );
+  }
+}
+
+export default RegisterForm;
