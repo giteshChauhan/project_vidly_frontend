a1b365467332acc3a272232bb54d0fdd8e45d30a
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-08-05 01:13:44 +0530

    added login form component

---
+++ src/components/common/loginForm.jsx
@@ -0,0 +1,34 @@
+import Joi from "joi-browser";
+import Form from "./form";
+
+class LoginForm extends Form {
+  state = {
+    data: { username: "", password: "" },
+    errors: {},
+  };
+
+  schema = {
+    username: Joi.string().required().label("Username"),
+    password: Joi.string().required().label("Password"),
+  };
+
+  doSubmit = () => {
+    // call the server
+    console.log("Submitted");
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Login</h1>
+        <form onSubmit={this.handleSubmit}>
+          {this.renderInput("username", "Username", "text", true)}
+          {this.renderInput("password", "Password", "password")}
+          {this.renderButton("Login")}
+        </form>
+      </div>
+    );
+  }
+}
+
+export default LoginForm;
