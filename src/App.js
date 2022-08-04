ae9c4a0ea5fe6401c9b46548f4fcf4ada255c86b
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-08-05 00:42:31 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import "./App.css";
+import { Route, Routes, Navigate } from "react-router-dom";
+import NavBar from "./components/navBar";
 import Movies from "./components/movies";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
+import MovieForm from "./components/movieForm";
+import LoginForm from "./components/common/loginForm";
+import "./App.css";
 
 function App() {
   return (
-    <main className="container">
-      <Movies />
-    </main>
+    <>
+      <NavBar />
+      <main className="container">
+        <Routes>
+          <Route path="/login" element={<LoginForm />}></Route>
+          <Route path="/movies/:id" element={<MovieForm />}></Route>
+          <Route path="/movies" element={<Movies />}></Route>
+          <Route path="/customers" element={<Customers />}></Route>
+          <Route path="/rentals" element={<Rentals />}></Route>
+          <Route path="/not-found" element={<NotFound />}></Route>
+          <Route
+            path="/"
+            element={<Navigate to={"/movies"} replace={true} />}
+          />
+          <Route
+            path="/:id"
+            element={<Navigate to={"/not-found"} replace={true} />}
+          />
+        </Routes>
+      </main>
+    </>
   );
 }
 
