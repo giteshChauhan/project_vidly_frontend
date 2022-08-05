805448c57f9896010eed3e351d35c2cf30c5f51a
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-08-05 19:39:35 +0530

    added select.jsx common component

---
+++ src/components/common/select.jsx
@@ -0,0 +1,18 @@
+const Select = ({ name, label, error, options, ...rest }) => {
+  return (
+    <div className="form-group">
+      <label htmlFor={name}>{label}</label>
+      <select {...rest} name={name} id={name} className="form-select">
+        <option value="" />
+        {options.map((option) => (
+          <option key={option._id} value={option._id}>
+            {option.name}
+          </option>
+        ))}
+      </select>
+      {error && <div className="alert alert-danger">{error}</div>}
+    </div>
+  );
+};
+
+export default Select;
