7b6a30e4fbbe4020ebe44b3b072b8826994665aa
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-08-08 21:39:54 +0530

    added toase container

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Route, Routes, Navigate } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import NavBar from "./components/navBar";
 import Movies from "./components/movies";
 import Customers from "./components/customers";
@@ -7,11 +8,13 @@ import NotFound from "./components/notFound";
 import MovieForm from "./components/movieForm";
 import LoginForm from "./components/common/loginForm";
 import RegisterForm from "./components/registerForm";
+import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 
 function App() {
   return (
     <>
+      <ToastContainer />
       <NavBar />
       <main className="container">
         <Routes>
