7b73d0969464eb6136b647ae5d1d7610be0b3aae
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-08-10 00:46:31 +0530

    updated App.jsx | changed loginForm path

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Customers from "./components/customers";
 import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
 import MovieForm from "./components/movieForm";
-import LoginForm from "./components/common/loginForm";
+import LoginForm from "./components/loginForm";
 import RegisterForm from "./components/registerForm";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
