bbb035d760945754c7ef1f68cabb05c782db62b6
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-08-10 01:16:18 +0530

    updated registerForm.jsx | added http register

--- src/components/registerForm.jsx
+++ src/components/registerForm.jsx
@@ -1,5 +1,6 @@
 import Form from "./common/form";
 import Joi from "joi-browser";
+import { register } from "./../services/userService";
 
 class RegisterForm extends Form {
   state = {
@@ -9,12 +10,20 @@ class RegisterForm extends Form {
 
   schema = {
     username: Joi.string().required().label("Username").email(),
-    password: Joi.string().required().label("Password").min(5),
+    password: Joi.string().required().label("Password"),
     name: Joi.string().required().label("Name"),
   };
 
-  doSubmit = () => {
-    console.log("Registered");
+  doSubmit = async () => {
+    try {
+      await register(this.state.data);
+    } catch (ex) {
+      if (ex.response && ex.response.status === 400) {
+        const errors = { ...this.state.errors };
+        errors.username = ex.response.data;
+        this.setState({ errors });
+      }
+    }
   };
 
   render() {
