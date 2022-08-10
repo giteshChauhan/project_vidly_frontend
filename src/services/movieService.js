5df2a92b4a16b6a4b28d969a1aae2b598b503135
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-08-10 16:40:26 +0530

    updated services | removed explicit tokenKey

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -2,45 +2,23 @@ import http from "./httpService";
 import config from "../config.json";
 
 const apiEndPoint = config.apiUrl + "/movies/";
-const auth_token =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MmE3MDM0NzM0YzM0MWQ5MzQ0ZTdkMmQiLCJpc0FkbWluIjp0cnVlLCJpYXQiOjE2NTUxMTI2MTV9.cPdtfBbrAd_FKzIA1lBkS01UVX-25IhTDt3CUdjHfCg";
 
 export function getMovies() {
-  return http.get(apiEndPoint, {
-    headers: {
-      "x-auth-token": auth_token,
-    },
-  });
+  return http.get(apiEndPoint);
 }
 
 export function getMovie(id) {
-  return http.get(apiEndPoint + id, {
-    headers: {
-      "x-auth-token": auth_token,
-    },
-  });
+  return http.get(apiEndPoint + id);
 }
 
 export function saveMovie(movie, movieId) {
   if (movieId === "new") {
-    return http.post(apiEndPoint, movie, {
-      headers: {
-        "x-auth-token": auth_token,
-      },
-    });
+    return http.post(apiEndPoint, movie);
   }
 
-  return http.put(apiEndPoint + movieId, movie, {
-    headers: {
-      "x-auth-token": auth_token,
-    },
-  });
+  return http.put(apiEndPoint + movieId, movie);
 }
 
 export function deleteMovie(id) {
-  return http.delete(apiEndPoint + id, {
-    headers: {
-      "x-auth-token": auth_token,
-    },
-  });
+  return http.delete(apiEndPoint + id);
 }
