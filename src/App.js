c31a751dde42cb708b551934649c2e9477954b49
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-08-10 15:53:29 +0530

    updated app.js | added Logout route and jwtToken

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import { useEffect, useState } from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
+
 import NavBar from "./components/navBar";
 import Movies from "./components/movies";
 import Customers from "./components/customers";
@@ -8,18 +10,29 @@ import NotFound from "./components/notFound";
 import MovieForm from "./components/movieForm";
 import LoginForm from "./components/loginForm";
 import RegisterForm from "./components/registerForm";
+import Logout from "./components/logout";
+import auth from "./services/authService";
+
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 
 function App() {
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    const user = auth.getCurrentUser();
+    setUser(user);
+  }, []);
+
   return (
     <>
       <ToastContainer />
-      <NavBar />
+      <NavBar user={user} />
       <main className="container">
         <Routes>
           <Route path="/register" element={<RegisterForm />} />
           <Route path="/login" element={<LoginForm />}></Route>
+          <Route path="/logout" element={<Logout />}></Route>
           <Route path="/movies/:id" element={<MovieForm />}></Route>
           <Route path="/movies" element={<Movies />}></Route>
           <Route path="/customers" element={<Customers />}></Route>
