7518c3a5ad2f0fcfff27bb17103ee0cb79963e25
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-08-10 15:54:21 +0530

    added authServices

--- src/components/registerForm.jsx
+++ src/components/registerForm.jsx
@@ -1,6 +1,7 @@
 import Form from "./common/form";
 import Joi from "joi-browser";
 import { register } from "./../services/userService";
+import auth from "./../services/authService";
 
 class RegisterForm extends Form {
   state = {
@@ -17,7 +18,7 @@ class RegisterForm extends Form {
   doSubmit = async () => {
     try {
       const response = await register(this.state.data);
-      localStorage.setItem("token", response.headers["x-auth-token"]);
+      auth.loginWithJwt(response.headers["x-auth-token"]);
       window.location = "/";
     } catch (ex) {
       if (ex.response && ex.response.status === 400) {
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,8 +1,38 @@
 import http from "./httpService";
 import config from "../config.json";
+import jwtDecode from "jwt-decode";
 
 const apiEndPoint = config.apiUrl + "/login/";
+const tokenKey = "token";
 
-export function login(email, password) {
-  return http.post(apiEndPoint, { email, password });
+export async function login(email, password) {
+  const { data: jwt } = await http.post(apiEndPoint, { email, password });
+  localStorage.setItem(tokenKey, jwt);
 }
+
+export function loginWithJwt(jwt) {
+  localStorage.setItem(tokenKey, jwt);
+}
+
+export function logout() {
+  localStorage.removeItem(tokenKey);
+}
+
+export function getCurrentUser() {
+  try {
+    const jwt = localStorage.getItem(tokenKey);
+    const user = jwtDecode(jwt);
+    return user;
+  } catch (ex) {
+    return undefined;
+  }
+}
+
+const auth = {
+  login,
+  loginWithJwt,
+  logout,
+  getCurrentUser,
+};
+
+export default auth;
