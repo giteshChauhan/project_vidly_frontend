aef97daab58742c76bdf187fdc15c1532b00a8b2
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-08-15 00:40:41 +0530

    updated genreServices.js | added CUD

--- src/services/genreService.js
+++ src/services/genreService.js
@@ -1,7 +1,21 @@
 import http from "./httpService";
+import auth from "./authService";
 
 const apiEndPoint = "/genres/";
+const token = auth.getJwt();
 
 export function getGenres() {
   return http.get(apiEndPoint);
+}
+
+export function deleteGenre(id) {
+  return http.delete(`${apiEndPoint}/${id}`, token);
+}
+
+export function addGenre(genre) {
+  return http.post(apiEndPoint, genre, token);
+}
+
+export function editGenre(genre) {
+  return http.put(apiEndPoint + genre._id, { name: genre.name }, token);
 }
