ec59c1b2cb90fb3c88df7c9a814f3c673205917b
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-08-16 19:04:27 +0530

    added app.css

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
 import "./css/index.css";
-import "./css/myCss.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
