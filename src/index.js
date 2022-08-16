5fea5034bde49278c972e5147c368799f19c5fce
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-08-16 19:01:27 +0530

    updated index.js | added myCss path

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import "./css/index.css";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
+import "./css/index.css";
+import "./css/myCss.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
