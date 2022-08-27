96f135aa6b864b50a40ef264007a7e4d3c4f2c20
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-08-27 20:56:57 +0530

    added fetchYoutubeVideos netlify function

---
+++ functions/fetchYoutubeVideos.js
@@ -0,0 +1,36 @@
+const axios = require("axios");
+
+exports.handler = async function (event, context) {
+  try {
+    const keyword = event.queryStringParameters.q;
+    const { data: videos } = await axios({
+      method: "get",
+      url: "https://youtube.googleapis.com/youtube/v3/search",
+      params: {
+        q: keyword,
+        part: "snippet",
+        maxResults: 6,
+        key: process.env.REACT_APP_YTAPI_KEY,
+      },
+      headers: {},
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(videos),
+    };
+  } catch (error) {
+    if (error.response) {
+      const { status, statusText, headers, data } = error.response;
+      return {
+        statusCode: status,
+        body: JSON.stringify({ status, statusText, headers, data }),
+      };
+    }
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify(error),
+    };
+  }
+};
