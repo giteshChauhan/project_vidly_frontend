9c151ee322235fbd848d0e9d86bd5d740ca5f579
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-08-27 20:57:45 +0530

    added video.jsx component

---
+++ src/components/video.jsx
@@ -0,0 +1,30 @@
+const Video = ({ data, onVideoSelected }) => {
+  return data.map(({ snippet, id }) => {
+    return (
+      <div
+        className="card"
+        style={{
+          width: "18rem",
+          margin: "3px",
+          boxShadow: "0 0  1px 1px #6e00ff",
+        }}
+        key={id.videoId}
+        onClick={() => onVideoSelected(id.videoId)}
+      >
+        <div className="crop-container">
+          <img
+            src={snippet.thumbnails.high.url}
+            alt={snippet.channelTitle}
+            className="card-img-top"
+          />
+        </div>
+        <div className="card-body">
+          <h5 className="card-title">{snippet.channelTitle}</h5>
+          <p className="card-text">{id.videoId}</p>
+        </div>
+      </div>
+    );
+  });
+};
+
+export default Video;
