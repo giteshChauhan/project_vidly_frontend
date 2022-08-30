4bd32fe74ec862639b861355f8d3006e698d9cf8
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-08-31 02:41:54 +0530

    updated customers.jsx | updated title

--- src/components/customers.jsx
+++ src/components/customers.jsx
@@ -1,6 +1,11 @@
 // Customers
+import { useEffect } from "react";
 
 const Customers = () => {
+  useEffect(() => {
+    document.title = "VIDLY | Customers";
+  }, []);
+
   return <h1>Customers</h1>;
 };
 
