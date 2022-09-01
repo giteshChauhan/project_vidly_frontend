7cde190c9c374dd1309dc0eec7ead1ab23808a64
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-09-01 18:11:21 +0530

    updated videoPlayer.jsx

--- src/components/common/videoPlayer.jsx
+++ src/components/common/videoPlayer.jsx
@@ -6,7 +6,7 @@ const Videoplayer = ({ videoId }) => {
       <iframe
         title={videoId}
         frameBorder="0"
-        src={`https://www.youtube.com/embed/${videoId}`}
+        src={`https://www.youtube.com/embed/${videoId}?modestbranding=1&iv_load_policy=3&rel=0`}
         allowFullScreen={true}
       />
     </div>
