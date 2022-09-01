0070cfbb6ee9aa6a949952aec1e45d7df133cae5
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-09-01 18:12:35 +0530

    updated moviesTable.jsx | added videoModal

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -6,6 +6,7 @@ import auth from "../services/authService";
 
 import ConfirmModal from "./common/confirmModal";
 import Table from "./common/table";
+import VideoModal from "./videoModal";
 
 import add_enable from "../icons/add_enable.png";
 import add_disable from "../icons/add_disable.png";
@@ -22,15 +23,19 @@ class MoviesTable extends Component {
       confirmText: "",
       data: {},
     },
+    videoModal: {
+      isOpen: false,
+      movie: {},
+    },
   };
   columns = [
     {
       path: "title",
       label: "Title",
       content: (movie) => (
-        <Link to={`/movies/${movie._id}`} style={{ textDecoration: "none" }}>
+        <div className="myTitle" onClick={() => this.handleVideoModal(movie)}>
           {movie.title}
-        </Link>
+        </div>
       ),
     },
     { path: "genre.name", label: "Genre" },
@@ -82,6 +87,20 @@ class MoviesTable extends Component {
     }
   };
 
+  addEditButton = (user) => {
+    if (user && user.isAdmin) {
+      const editButton = {
+        key: "edit",
+        content: (movie) => (
+          <Link to={`/movies/${movie._id}`} style={{ textDecoration: "none" }}>
+            <img src={edit_icon} className="myImg" alt={movie.title} />
+          </Link>
+        ),
+      };
+      this.columns.push(editButton);
+    }
+  };
+
   addDeleteButton = (user) => {
     if (user && user.isAdmin) {
       this.columns.push({
@@ -117,24 +136,26 @@ class MoviesTable extends Component {
     this.setState({ confirmModal });
   };
 
-  addEditButton = (user) => {
-    if (user && user.isAdmin) {
-      const editButton = {
-        key: "edit",
-        content: (movie) => (
-          <Link to={`/movies/${movie._id}`} style={{ textDecoration: "none" }}>
-            <img src={edit_icon} className="myImg" alt={movie.title} />
-          </Link>
-        ),
-      };
-      this.columns.push(editButton);
-    }
+  handleClose = () => {
+    const videoModal = {
+      isOpen: false,
+    };
+    this.setState({ videoModal });
+  };
+
+  handleVideoModal = (movie) => {
+    const videoModal = {
+      isOpen: true,
+      movie,
+    };
+    this.setState({ videoModal });
   };
 
   render() {
     const { movies, onSort, sortColumn } = this.props;
     const { title, isOpen, body, confirmStyle, confirmText, data } =
       this.state.confirmModal;
+    const { isOpen: openVideo, movie } = this.state.videoModal;
 
     return (
       <>
@@ -152,6 +173,11 @@ class MoviesTable extends Component {
           confirmText={confirmText}
           onConfirm={() => this.props.onDelete(data)}
           onCloseModal={this.handleCloseModal}
+        />
+        <VideoModal
+          isOpen={openVideo}
+          onClose={this.handleClose}
+          movie={movie}
         />
       </>
     );
