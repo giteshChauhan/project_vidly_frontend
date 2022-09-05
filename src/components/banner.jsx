4f849d6971d7917327f0bb87f11a1e7fd6b5f6ae
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-09-05 08:04:35 +0530

    added banner.jsx component

---
+++ src/components/banner.jsx
@@ -0,0 +1,25 @@
+import bannerPV_icon from "../icons/bannerPV_icon.png";
+import chill_icon from "../icons/chill_icon.png";
+
+const Banner = () => {
+  return (
+    <div className="myBanner">
+      <img src={bannerPV_icon} alt="bannerIcon" className="myBannerIcon" />
+      <h1 className="myBannerTitle">Project_Vidly</h1>
+      <p className="myBannerText">
+        Rent over 250+ top tier movies.
+        <img
+          src={chill_icon}
+          alt="Chill"
+          className="myImg"
+          style={{ marginRight: "2px" }}
+        />
+        Enjoy public movies ads-free.
+        <br className="myBr" />
+        Click about button for more details.
+      </p>
+    </div>
+  );
+};
+
+export default Banner;
