796b2b6e3ae93e6a3976510df866d2fe6fc45f18
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-09-28 20:30:31 +0530

    updated movie.jsx | added onMovieSelect

--- src/components/movie.jsx
+++ src/components/movie.jsx
@@ -1,7 +1,15 @@
 import movieLogo from "../icons/PV.png";
 
-const Movie = ({ data }) => {
-  return data.map(({ id, title, year, image, titleType }) => {
+const separateId = (id) => {
+  const firstIndex = id.search("e") + 2;
+  const lastIndex = id.lastIndexOf();
+  return id.slice(firstIndex, lastIndex);
+};
+
+const Movie = ({ data, onMovieSelected }) => {
+  return data.map((movie) => {
+    const { id, title, year, image, titleType } = movie;
+    const newId = separateId(id);
     let url = "";
     try {
       url = image.url;
@@ -15,7 +23,8 @@ const Movie = ({ data }) => {
           width: "18rem",
           margin: "3px",
         }}
-        key={id}
+        onClick={() => onMovieSelected({ newId, title, year, url })}
+        key={newId}
       >
         <div className="crop-container">
           <img src={url} alt={title} className="card-img-top" />
@@ -23,7 +32,7 @@ const Movie = ({ data }) => {
         <div className="card-body">
           <h5 className="card-title">{title}</h5>
           <p className="card-text">
-            {id}, {year}, {titleType}
+            {newId}, {year}, {titleType}
           </p>
         </div>
       </div>
