60431282c59168a198f18515a368e11ba11cea7a
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-10-05 01:57:17 +0530

    updated history.jsx | extracted apiCalls

--- src/components/history.jsx
+++ src/components/history.jsx
@@ -1,39 +1,17 @@
-import { useEffect, useCallback, useState } from "react";
+import { useEffect } from "react";
 import moment from "moment";
-import _ from "lodash";
 
 import Card from "./common/card";
 import MyDate from "./common/myDate";
 
-import { getHistory } from "../services/historyService";
-import { getMovie } from "../services/movieService";
-
 import chill_icon from "../icons/chill_icon.png";
 
-const History = () => {
-  const [moviesData, setMoviesData] = useState([]);
+const History = ({ moviesData }) => {
   let history = null;
-
-  const fetchWatchLaterMovieIds = useCallback(async () => {
-    const { data } = await getHistory();
-    fetchMovieById(data);
-  }, []);
-
-  const fetchMovieById = async (movieIds) => {
-    let movieData = [];
-    for (let movie of movieIds) {
-      const id = movie.movieId;
-      const { data } = await getMovie(id);
-      movieData.push({ movie, data });
-    }
-    _.reverse(movieData);
-    setMoviesData(movieData);
-  };
 
   useEffect(() => {
     document.title = "VIDLY | History";
-    fetchWatchLaterMovieIds();
-  }, [fetchWatchLaterMovieIds]);
+  }, []);
 
   return (
     <div style={{ marginLeft: "7%" }}>
@@ -46,7 +24,7 @@ const History = () => {
             show = true;
           }
           return (
-            <div key={movie._id}>
+            <div key={movie._id} style={{ marginBottom: "8px" }}>
               {show ? <MyDate date={movie.addedOn} /> : null}
               <Card body={movie} movie={data} bodyText="Watched On" />
             </div>
