42e85d4a064776e091d55b14f8c4fbc9950e5b3d
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-10-05 22:37:28 +0530

    updated header.jsx | added marquee

--- src/components/common/header.jsx
+++ src/components/common/header.jsx
@@ -1,15 +1,18 @@
+import Marquee from "react-fast-marquee";
 import chill_icon from "../../icons/chill_icon.png";
 
 const Header = ({ id = null }) => {
   return (
-    <div className="myModal myModalFlex" id={id}>
-      <img src={chill_icon} alt="chill" className="myImg" />
-      <h6
-        className="modal-title "
-        style={{ color: "#6e00ff", marginLeft: "4px" }}
-      >
-        Enjoy add-free content
-      </h6>
+    <div className="myModalFlex" id={id}>
+      <Marquee direction="right" speed={40} gradient={false}>
+        <img src={chill_icon} alt="chill" className="myImg" />
+        <h6
+          className="modal-title "
+          style={{ color: "#6e00ff", fontSize: "17px" }}
+        >
+          Enjoy ad-free content
+        </h6>
+      </Marquee>
     </div>
   );
 };
