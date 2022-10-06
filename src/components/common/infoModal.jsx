1228c92adae688be2b2c1b60158d233fd35e4c7c
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-10-07 00:32:40 +0530

    updated infoModal.jsx

--- src/components/common/infoModal.jsx
+++ src/components/common/infoModal.jsx
@@ -1,34 +1,42 @@
 import { useState } from "react";
 
-const InfoModal = ({ data }) => {
+const InfoModal = ({ data, toOpen, onExit, headerText }) => {
   const [isOpen, setIsOpen] = useState(true);
 
   const onClose = () => {
     setIsOpen(false);
   };
   return (
-    <div style={isOpen ? null : { display: "none" }} className="modalContainer">
+    <div
+      style={isOpen || toOpen ? null : { display: "none" }}
+      className="modalContainer"
+    >
       <div className="modal-dialog modal-lg myModal">
         <div className="modal-content">
-          <div className="myModalFlex">
+          <div className="myModalFlex" style={{ alignItems: "center" }}>
+            <h4 className="myTitle">{headerText}</h4>
             <div className="divider"></div>
             <button
               type="button"
               className="myCloseBtnMinimal m-2"
               onClick={() => {
-                onClose();
+                if (toOpen) onExit();
+                else onClose();
               }}
             >
               X
             </button>
           </div>
-          <div className="my-modal-body" style={{ paddingTop: "7%" }}>
-            {data}
-          </div>
+          <div className="my-modal-body">{data}</div>
         </div>
       </div>
     </div>
   );
+};
+
+InfoModal.defaultProps = {
+  toOpen: false,
+  headerText: null,
 };
 
 export default InfoModal;
